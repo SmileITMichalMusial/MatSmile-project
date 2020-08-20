f4069a963a301fae8fdb7282664de13aec23c5ef
Michał Musiał <34947682+michalolafmusial@users.noreply.github.com>
2020-08-21 01:32:59 +0200

    20200817

--- src/main/java/pl/matemania/dao/TopicsDao.java
+++ src/main/java/pl/matemania/dao/TopicsDao.java
@@ -3,6 +3,7 @@ package pl.matemania.dao;
 import pl.matemania.domain.topics.TopicLayer1;
 import pl.matemania.domain.topics.TopicLayer2;
 import pl.matemania.domain.topics.TopicLayer3;
+import pl.matemania.domain.topics.TopicLayer4;
 
 import java.util.List;
 
@@ -11,9 +12,11 @@ public interface TopicsDao {
     TopicLayer1 getSingleTopicLayer1 (int id);
     TopicLayer2 getSingleTopicLayer2 (int id);
     TopicLayer3 getSingleTopicLayer3 (int id);
+    TopicLayer4 getSingleTopicLayer4 (int id);
 
     List<TopicLayer1> getTopicLayer1FromDbSortedByOrderId();
     List<TopicLayer2> getTopicLayer2FromDbSortedByOrderId();
     List<TopicLayer3> getTopicLayer3FromDbSortedByOrderId();
+    List<TopicLayer4> getTopicLayer4FromDbSortedByOrderId();
 
 }
